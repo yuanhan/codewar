61778aaf947fdb09be9a0cfaf23ab9e02c667ff8
Han YUAN <yuanhan1992@gmail.com>
2016-10-02 00:08:02 -0700

    how to combine strings

---
+++ Like.py
@@ -0,0 +1,42 @@
+# Who likes it?
+# 6kyu
+# https://www.codewars.com/kata/who-likes-it/python
+
+'''
+Description:
+
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+
+likes [] // must be "no one likes this"
+likes ["Peter"] // must be "Peter likes this"
+likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+'''
+
+
+def likes(names):
+    if names == []:
+        return 'no one likes this'
+    elif len(names) == 1:
+        return names[0] + ' likes this'
+    elif len(names) == 2:
+        return names[0] + ' and ' + names[1] + ' like this'
+    elif len(names) == 3:
+        return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'
+    else:
+        return names[0] + ', ' + names[1] + ' and ' + \
+            str(len(names) - 2) + ' others like this'
+
+
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this',
+        2: '{} and {} like this',
+        3: '{}, {} and {} like this',
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n - 2)
